50a27d978f76340c811fc1a21a59e4296a9fff67
hudejie <hudejie2018#163.com>
2021-06-04 17:05:18 +0800

    增加动态云

--- include/catpane.h
+++ include/catpane.h
@@ -2,6 +2,7 @@
 #define CCATPANE_H
 
 #include <QWidget>
+#include <QTimer>
 
 class CCatPane : public QWidget
 {
@@ -15,7 +16,11 @@ protected:
 private:
     void mouseMoveEvent(QMouseEvent *event);
 
+    void DrawCloud(QPainter *p, QRect rectMain);
     void DrawCat(QPainter *p, QRect rectMain);
+
+public slots:
+    void OnTimerCloudTimeOut();
 
 private:
     double m_fXRadio;
@@ -25,6 +30,18 @@ private:
     QPixmap m_pixCatEyeWrite;
     QPixmap m_pixCatEyeBlack;
     QPixmap m_pixCatEyeBg;
+
+    double m_nScaleCloud1;
+    double m_nScaleCloud2;
+    double m_nScaleCloud3;
+
+    QPixmap m_pixCloud001;
+    QPixmap m_pixCloud002;
+    QPixmap m_pixCloud003;
+    QPixmap m_pixCloud004;
+    QPixmap m_pixSun;
+
+    QTimer *m_pCloudTimer;
 };
 
 #endif // CCATPANE_H
--- source/catpane.cpp
+++ source/catpane.cpp
@@ -9,15 +9,29 @@ CCatPane::CCatPane(QWidget *parent) : QWidget(parent)
 {
     m_fXRadio = 0.5;
     m_fYRadio = 0.5;
+    m_nScaleCloud1 = 0;
+    m_nScaleCloud2 = 0;
+    m_nScaleCloud3 = 0;
 
     m_pixCat.load(":/qss/res/img/cat/cat.png");
     m_pixCatEyeWrite.load(":/qss/res/img/cat/eye_write.png");
     m_pixCatEyeBlack.load(":/qss/res/img/cat/eye_black.png");
     m_pixCatEyeBg.load(":/qss/res/img/cat/eye_bg.png");
+
+    m_pixCloud001.load(":/qss/res/img/cat/cuteCloud001.png");
+    m_pixCloud002.load(":/qss/res/img/cat/cuteCloud002.png");
+    m_pixCloud003.load(":/qss/res/img/cat/cuteCloud003.png");
+    m_pixCloud004.load(":/qss/res/img/cat/cuteCloud004.png");
+    m_pixSun.load(":/qss/res/img/cat/cuteCloud005.png");
+
     setMouseTracking(true);
 
     QPixmap myCursor = QPixmap(":/qss/res/img/cat/feather.png").scaled(QSize(40, 40), Qt::KeepAspectRatio, Qt::SmoothTransformation);
     setCursor(myCursor);
+
+    m_pCloudTimer = new QTimer(this);
+    connect(m_pCloudTimer, SIGNAL(timeout()), this, SLOT(OnTimerCloudTimeOut()));
+    m_pCloudTimer->start(100);
 }
 
 void CCatPane::paintEvent(QPaintEvent *)
@@ -25,11 +39,13 @@ void CCatPane::paintEvent(QPaintEvent *)
     QPainter painter(this);
     painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
 
+    DrawCloud(&painter, rect());
+
     double fRadio = (double)1080.0 / 1920.0;
     QRect rectCat = rect();
     rectCat.setTop(rectCat.height() >> 1);
     int nWidth = rectCat.height() / fRadio;
-    rectCat.setLeft(rectCat.left() + (rectCat.width() - nWidth) >> 1);
+    rectCat.setLeft(rectCat.left() + ((rectCat.width() - nWidth) >> 1));
     rectCat.setRight(rectCat.left() + nWidth);
     DrawCat(&painter, rectCat);
 }
@@ -40,6 +56,25 @@ void CCatPane::mouseMoveEvent(QMouseEvent *event)
     m_fYRadio = (double)event->pos().y() / height();
 
     update();
+}
+
+void CCatPane::DrawCloud(QPainter *painter, QRect rectMain)
+{
+    painter->drawPixmap(rectMain.left() + 30, rectMain.top() + 10, m_pixSun);
+    painter->drawPixmap(rectMain.right() - 200, rectMain.top() + 10, m_pixCloud003);
+
+    if (m_nScaleCloud1 > rectMain.width())
+        m_nScaleCloud1 = -m_pixCloud001.width();
+
+    if (m_nScaleCloud2 > rectMain.width())
+        m_nScaleCloud2 = -m_pixCloud002.width();
+
+    if (m_nScaleCloud3 > rectMain.width())
+        m_nScaleCloud3 = -m_pixCloud004.width();
+
+    painter->drawPixmap(m_nScaleCloud1, rectMain.top() + 60, m_pixCloud001);
+    painter->drawPixmap(m_nScaleCloud2, rectMain.top() + 60, m_pixCloud002);
+    painter->drawPixmap(m_nScaleCloud3, rectMain.top() + 70, m_pixCloud004);
 }
 
 void CCatPane::DrawCat(QPainter *painter, QRect rectMain)
@@ -80,4 +115,13 @@ void CCatPane::DrawCat(QPainter *painter, QRect rectMain)
     painter->drawPixmap(rcImgCat, p);
 
     painter->restore();
+}
+
+void CCatPane::OnTimerCloudTimeOut()
+{
+    m_nScaleCloud2 += 0.1;
+    m_nScaleCloud3 = m_nScaleCloud2 + 80;
+    m_nScaleCloud1 = m_nScaleCloud3 + 300;
+
+    update();
 }
